fd8a6f241b2adc6479deef7325c48534296f6172
Bambang Wicaksono <bambanx@live.com>
2024-10-25 06:50:50 -0400

    setup gh-pages - fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Ap
-p from './App.jsx'
+import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
